fb3e89407d902a2a64683b1a1871054e6e71b0f4
JEFFREY KEYSER <jkeyser@wisc.edu>
2020-04-13 21:06:59 -0500

    Initial Commit. Added Identity

---
+++ BugTracker/Areas/Identity/Data/BugTrackerUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BugTracker.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the BugTrackerUser class
+    public class BugTrackerUser : IdentityUser
+    {
+        [PersonalData]
+        public string FirstName { get; set; }
+    }
+}
---
+++ BugTracker/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BugTracker.Areas.Identity.Data;
+using BugTracker.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BugTracker.Areas.Identity.IdentityHostingStartup))]
+namespace BugTracker.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BugTrackerContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BugTrackerContextConnection")));
+
+             //  services.AddDefaultIdentity<BugTrackerUser>(options => options.SignIn.RequireConfirmedAccount = true)
+             //       .AddEntityFrameworkStores<BugTrackerContext>();
+            });
+        }
+    }
+}
